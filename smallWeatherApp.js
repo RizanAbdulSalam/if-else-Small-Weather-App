20732207a7605892b2d47eda7d4746412abcd02e
RizanAbdulSalam <82836256+RizanAbdulSalam@users.noreply.github.com>
2022-09-10 12:49:38 +0530

    Update smallWeatherApp.js

--- smallWeatherApp.js
+++ smallWeatherApp.js
@@ -1,6 +1,7 @@
 // Weather app
 
-let weather = 'sun' //this statement gives (the else case)
+let weather = 'sun' //console logging this statement will give the inside of (the else case)
+let weather = 'rain' //console logging this statement will give the inside of (the if case)  
 if (weather == 'rain'){
     console.log("Carry your umbrella with you!☔");
 } else{
